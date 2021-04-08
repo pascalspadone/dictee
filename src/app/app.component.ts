48becf0dc4a63439c5abf2fec68002388517a282
Pascal Spadone <pascal.spadone@gmail.com>
2021-04-08 21:53:54 +0200

    added sounds and images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'dictee';
+
+  playSound(): void {
+    const audio = new Audio();
+    audio.addEventListener('canplaythrough', () => audio.play());
+    audio.src = 'assets/sound/c-4.mp3';
+  }
 }
